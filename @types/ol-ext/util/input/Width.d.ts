ee472bea181725f46af187eb28a2b1e18503274a
Ko Nagase <nagase@georepublic.co.jp>
2021-10-17 14:42:11 +0900

    Added Options class inheritance with fixing jsdoc comment for util/input

--- @types/ol-ext/util/input/Width.d.ts
+++ @types/ol-ext/util/input/Width.d.ts
@@ -1,6 +1,6 @@
-import Slider from "./Slider";
+import List, { Options as ListOptions } from "./List";
 
-export interface Options {
+export interface Options extends ListOptions {
     className?: string;
     input?: Element;
     parent?: Element;
@@ -9,14 +9,14 @@ export interface Options {
 
 /** Checkbox input
  * @constructor
- * @extends {ol_ext_input_Slider}
+ * @extends {ol_ext_input_List}
  * @param {*} options
  *  @param {string} [options.className]
  *  @param {Element} [options.input] input element, if non create one
  *  @param {Element} [options.parent] parent element, if create an input
  *  @param {Array<number>} [options.size] a list of size (default 0,1,2,3,5,10,15,20)
  */
-export default class Width extends Slider {
+export default class Width extends List {
     constructor(options?: Options);
     /** Get the current value
      * @returns {number}
