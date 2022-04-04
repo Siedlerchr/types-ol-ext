ab28425ffe4b2ecbda61a8e42afe08a70fecf94d
Taro Matsuzawa aka. btm <btm@tech.email.ne.jp>
2022-04-04 11:51:09 +0900

    use OnSignature

--- @types/ol-ext/interaction/FocusMap.d.ts
+++ @types/ol-ext/interaction/FocusMap.d.ts
@@ -3,6 +3,13 @@ import { Map as _ol_Map_ } from 'ol';
 import BaseEvent from 'ol/events/Event';
 import { EventsKey } from 'ol/events';
 import { ObjectEvent } from 'ol/Object';
+import { CombinedOnSignature, EventTypes, OnSignature } from 'ol/Observable';
+import { Types } from 'ol/ObjectEventType';
+
+type FocusMapOnSignature<Return> = OnSignature<EventTypes, Event, Return> &
+  OnSignature<Types | 'change' | 'change:active' | 'error' | 'propertychange', ObjectEvent, Return> &
+  OnSignature<Types | 'focus', FocusEvent, Return> &
+  CombinedOnSignature<Types | EventTypes | 'change' | 'change:active' | 'error' | 'propertychange' | 'focus', Return>;
 
 /** An interaction to focus on the map on click. Usefull when using keyboard event on the map.
  * @constructor
@@ -13,25 +20,10 @@ export default class FocusMap extends Interaction {
     /** Set the map > add the focus button and focus on the map when pointerdown to enable keyboard events.
      */
     setMap(map: _ol_Map_): void;
-    on(type: string | string[], listener: (p0: any) => any): EventsKey | EventsKey[];
-    once(type: string | string[], listener: (p0: any) => any): EventsKey | EventsKey[];
-    un(type: string | string[], listener: (p0: any) => any): void;
-    on(type: 'change', listener: (evt: BaseEvent) => void): EventsKey;
-    once(type: 'change', listener: (evt: BaseEvent) => void): EventsKey;
-    un(type: 'change', listener: (evt: BaseEvent) => void): void;
-    on(type: 'change:active', listener: (evt: ObjectEvent) => void): EventsKey;
-    once(type: 'change:active', listener: (evt: ObjectEvent) => void): EventsKey;
-    un(type: 'change:active', listener: (evt: ObjectEvent) => void): void;
-    on(type: 'error', listener: (evt: BaseEvent) => void): EventsKey;
-    once(type: 'error', listener: (evt: BaseEvent) => void): EventsKey;
-    un(type: 'error', listener: (evt: BaseEvent) => void): void;
-    on(type: 'propertychange', listener: (evt: ObjectEvent) => void): EventsKey;
-    once(type: 'propertychange', listener: (evt: ObjectEvent) => void): EventsKey;
-    un(type: 'propertychange', listener: (evt: ObjectEvent) => void): void;
 
-    on(type: 'focus', listener: (evt: FocusEvent) => void): EventsKey;
-    once(type: 'focus', listener: (evt: FocusEvent) => void): EventsKey;
-    un(type: 'focus', listener: (evt: FocusEvent) => void): void;
+    on: FocusMapOnSignature<EventsKey>;
+    once: FocusMapOnSignature<EventsKey>;
+    un: FocusMapOnSignature<void>;
 }
 export class FocusEvent extends BaseEvent {
     constructor(type: 'focus');
