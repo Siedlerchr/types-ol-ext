0cf589b7614005da350ff0ff3a8d220ad6f2064f
Taro Matsuzawa aka. btm <btm@tech.email.ne.jp>
2022-04-04 11:19:40 +0900

    use OnSignature

--- @types/ol-ext/interaction/Split.d.ts
+++ @types/ol-ext/interaction/Split.d.ts
@@ -9,6 +9,14 @@ import VectorSource from 'ol/source/Vector';
 import BaseEvent from 'ol/events/Event';
 import { EventsKey } from 'ol/events';
 import { ObjectEvent } from 'ol/Object';
+import { CombinedOnSignature, EventTypes, OnSignature } from 'ol/Observable';
+import { Types } from 'ol/ObjectEventType';
+
+type SplitOnSignature<Return> = OnSignature<EventTypes, Event, Return> &
+  OnSignature<Types | 'change' | 'change:active' | 'error' | 'propertychange', ObjectEvent, Return> &
+  OnSignature<Types | 'beforesplit' | 'aftersplit', SplitEvent, Return> &
+  CombinedOnSignature<Types | EventTypes | 'change' | 'change:active' | 'error' | 'propertychange' | 'beforesplit' | 'aftersplit', Return>
+
 
 export enum SplitEventType {
     BEFORESPLIT = 'beforesplit',
@@ -65,29 +73,9 @@ export default class Split extends Interaction {
      * @param {MapBrowserEvent} evt Event.
      */
     handleMoveEvent(evt: MapBrowserEvent): void;
-
-    on(type: string | string[], listener: (p0: any) => any): EventsKey | EventsKey[];
-    once(type: string | string[], listener: (p0: any) => any): EventsKey | EventsKey[];
-    un(type: string | string[], listener: (p0: any) => any): void;
-    on(type: 'change', listener: (evt: BaseEvent) => void): EventsKey;
-    once(type: 'change', listener: (evt: BaseEvent) => void): EventsKey;
-    un(type: 'change', listener: (evt: BaseEvent) => void): void;
-    on(type: 'change:active', listener: (evt: ObjectEvent) => void): EventsKey;
-    once(type: 'change:active', listener: (evt: ObjectEvent) => void): EventsKey;
-    un(type: 'change:active', listener: (evt: ObjectEvent) => void): void;
-    on(type: 'error', listener: (evt: BaseEvent) => void): EventsKey;
-    once(type: 'error', listener: (evt: BaseEvent) => void): EventsKey;
-    un(type: 'error', listener: (evt: BaseEvent) => void): void;
-    on(type: 'propertychange', listener: (evt: ObjectEvent) => void): EventsKey;
-    once(type: 'propertychange', listener: (evt: ObjectEvent) => void): EventsKey;
-    un(type: 'propertychange', listener: (evt: ObjectEvent) => void): void;
-
-    on(type: 'beforesplit', listener: (evt: SplitEvent) => void): EventsKey;
-    once(type: 'beforesplit', listener: (evt: SplitEvent) => void): EventsKey;
-    un(type: 'beforesplit', listener: (evt: SplitEvent) => void): void;
-    on(type: 'aftersplit', listener: (evt: SplitEvent) => void): EventsKey;
-    once(type: 'aftersplit', listener: (evt: SplitEvent) => void): EventsKey;
-    un(type: 'aftersplit', listener: (evt: SplitEvent) => void): void;
+    on: SplitOnSignature<EventsKey>;
+    once: SplitOnSignature<EventsKey>;
+    un: SplitOnSignature<void>;
 }
 export class SplitEvent extends BaseEvent {
     constructor(type: SplitEventType,
