fd92c93673a3b8ab06d3460ad5e4e2d41fa3f6cf
Siedlerchr <siedlerkiller@gmail.com>
2022-08-09 20:04:13 +0200

    Update to new ol-ext version
    Add new properties and methods

--- @types/ol-ext/control/Graticule.d.ts
+++ @types/ol-ext/control/Graticule.d.ts
@@ -12,25 +12,30 @@ export interface Options extends ControlOptions {
     step?: number;
     stepCoord?: number;
     spacing?: number;
+    intervals?: number[];
+    precison?: number;
     borderWidth?: number;
     margin?: number;
     formatCoord?: (coordinate: Coordinate, position: position) => string;
 }
 
-/**
- * Draw a graticule on the map.
- *
+/** Draw a graticule on the map.
  * @constructor
- * @extends {contrCanvasBase}
+ * @author mike-000 https://github.com/mike-000
+ * @author Jean-Marc Viglino https://github.com/viglino
+ * @extends {ol_control_CanvasBase}
  * @param {Object=} _ol_control_ options.
- *  @param {projectionLike} options.projection projection to use for the graticule, default EPSG:4326
+ *  @param {ol.projectionLike} options.projection projection to use for the graticule, default EPSG:4326
  *  @param {number} options.maxResolution max resolution to display the graticule
- *  @param {Style} options.style Style to use for drawing the graticule, default black.
- *  @param {number} options.step step beetween lines (in proj units), default 1
+ *  @param {ol_style_Style} options.style Style to use for drawing the graticule, default black.
+ *  @param {number} options.step step between lines (in proj units), default 1
  *  @param {number} options.stepCoord show a coord every stepCoord, default 1
- *  @param {number} options.spacing spacing beetween lines (in px), default 40px
+ *  @param {number} options.spacing spacing between lines (in px), default 40px
+ *  @param {Array<number>} options.intervals array (in desending order) of intervals (in proj units) constraining which lines will be displayed, default is no contraint (any multiple of step can be used)
+ *  @param {number} options.precision precision interval (in proj units) of displayed lines, if the line interval exceeds this more calculations will be used to display curved lines more accurately
  *  @param {number} options.borderWidth width of the border (in px), default 5px
  *  @param {number} options.margin margin of the border (in px), default 0px
+ *  @param {number} options.formatCoord a function that takes a coordinate and a position and return the formated coordinate
  */
 export default class Graticule extends CanvasBase {
     constructor(options?: Options);
