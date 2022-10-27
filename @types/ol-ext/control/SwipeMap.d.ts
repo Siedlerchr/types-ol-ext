ecffd3e4deee4c8fc40fa447db9295c1c667dca2
Felix <10731299+felhe@users.noreply.github.com>
2022-10-27 18:47:38 +0200

    Fix types for swipe & swipe-map control (#109)
    * docs: update types for layers in swipe control options
    
    * docs: add missing option to swipe map control

--- @types/ol-ext/control/SwipeMap.d.ts
+++ @types/ol-ext/control/SwipeMap.d.ts
@@ -1,14 +1,12 @@
 import { Map as _ol_Map_ } from "ol";
 import { Extent } from 'ol/extent';
 import ol_control_Control, { Options as ControlOptions } from "ol/control/Control";
-import { Layer } from "ol/layer";
 
 export interface Options extends ControlOptions{
-    layer?: Layer;
-    rightLayer?: Layer;
     className?: string;
     position?: number[],
-    orientaton?: 'vertical'| 'horizontal'
+    orientation?: 'vertical'| 'horizontal',
+    right?: boolean;
 }
 
 /** A control that use a CSS clip rect to swipe the map
@@ -17,11 +15,10 @@ export interface Options extends ControlOptions{
  * @constructor
  * @extends {ol_control_Control}
  * @param {Object=} Control options.
- *  @param {ol.layer} options.layers layer to swipe
- *  @param {ol.layer} options.rightLayer layer to swipe on right side
  *  @param {string} options.className control class name
- *  @param {number} options.position position propertie of the swipe [0,1], default 0.5
- *  @param {string} options.orientation orientation propertie (vertical|horizontal), default vertical
+ *  @param {number} options.position position property of the swipe [0,1], default 0.5
+ *  @param {string} options.orientation orientation property (vertical|horizontal), default vertical
+ *  @param {boolean} options.right true to position map on right side (resp. bottom for horizontal orientation), default false
  */
 export default class SwipeMap extends ol_control_Control {
     constructor(options: any);
